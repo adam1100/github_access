b291906044185a192e048b6b24764463d919f9b8
Adam Paul Lysaght <alysaght@tcd.ie>
2020-12-08 16:57:15 +0000

    successful fetching of github data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            items: [],
+            isLoaded: false
+        }
+
+    }
+
+    componentDidMount() {
+
+        fetch('https://api.github.com/users/adam1100/repos')
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    items: json,
+                    isLoaded: true, 
+                })
+            }).catch((err) => {
+                console.log(err);
+            });
+
+    }
+
+    render() {
+
+        const { isLoaded, items } = this.state;
+
+        if (!isLoaded)
+            return <div>Loading...</div>;
+
+        return (
+            <div className="App">
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            Name: {item.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+
+    }
+
 }
 
 export default App;
