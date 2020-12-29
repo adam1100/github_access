96bc33dfd11bcb035e4a5d69cb0f310682c459da
Adam Paul Lysaght <alysaght@tcd.ie>
2020-12-29 17:54:21 +0000

    added text box to input user api key

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Form from "./components/Form";
 import Graph from "./components/Graph";
 import './App.css';
 
-require('dotenv').config()
-
-const token = 'API Token'
-const headers = {
-    "Authorization" : "Token " + token
-};
-
 class App extends React.Component {
 
     constructor(props) {
@@ -21,50 +14,55 @@ class App extends React.Component {
             user: '',
             commitData: [],
             commitDataLoaded: false,
-            contributors: []
+            contributors: [],
+            token: ''
         }
     }
 
     getUser = async (e) => {
+    
         e.preventDefault();
         const username = e.target.elements.userName.value;
+        const apiKey = e.target.elements.apiKey.value;
         await this.setState({
             user: username,
-        }) 
+            token: apiKey,
+        })
+
         console.log("State user: ", this.state.user);
         e.preventDefault();
         var repos3 = await this.pageRequest
-        (`https://api.github.com/users/${this.state.user}/repos?&per_page=100&page=`);
+            (`https://api.github.com/users/${this.state.user}/repos?&per_page=100&page=`);
         this.setState({
             repos: repos3,
-        }) 
-
+        })
+        
         const repos = this.state.repos;
         var commitData = [];
         console.log(repos.length)
-       
-        for(var i = 0; i < repos.length; i++) {
-        var repoName = repos[i].name;
-        console.log(repoName);
 
-   
-        var commitArray = await this.pageRequest
-        (`https://api.github.com/repos/${this.state.user}/${repoName}/commits?&per_page=100&page=`);
+        for (var i = 0; i < repos.length; i++) {
+            var repoName = repos[i].name;
+            console.log(repoName);
 
-        var contributorsArray  = await this.pageRequest
-        (`https://api.github.com/repos/${this.state.user}/${repoName}/contributors?&per_page=100&page=`);
 
-        var data = { repoName: repoName, commits: commitArray.length, contributors: contributorsArray.length };
-        commitData.push(data);
-    }
+            var commitArray = await this.pageRequest
+                (`https://api.github.com/repos/${this.state.user}/${repoName}/commits?&per_page=100&page=`);
+
+            var contributorsArray = await this.pageRequest
+                (`https://api.github.com/repos/${this.state.user}/${repoName}/contributors?&per_page=100&page=`);
+
+            var data = { repoName: repoName, commits: commitArray.length, contributors: contributorsArray.length };
+            commitData.push(data);
+        }
 
         this.setState({
             isLoaded: true,
             commitData: commitData,
         })
-      }
+    }
 
-    async pageRequest(url){
+    async pageRequest(url) {
         const baseUrl = url;
         var size;
         let finished = false;
@@ -72,45 +70,45 @@ class App extends React.Component {
         let repos1 = [];
         let lastResult = [];
         do {
-          try {
-            const resp = await fetch(`${baseUrl}${page}`, {
-                method: 'GET',
-                headers: headers,
-              });
+            try {
+                const resp = await fetch(`${baseUrl}${page}`, {
+                    method: 'GET',
+                    headers: {"Authorization": "Token " + this.state.token}
+                });
 
-            const data = await resp.json();
-            lastResult = data;
-            repos1.push(data);
-            size = lastResult.length;
-            page++;
+                const data = await resp.json();
+                lastResult = data;
+                repos1.push(data);
+                size = lastResult.length;
+                page++;
 
-          } catch (err) {
-            console.error(`Oeps, sometshing is wrong ${err}`);
-          }
-          if(size < 100){
-              finished = true;
-          }
-        } while (!finished );
+            } catch (err) {
+                console.error(`Oeps, sometshing is wrong ${err}`);
+            }
+            if (size < 100) {
+                finished = true;
+            }
+        } while (!finished);
         var repos2 = [].concat.apply([], repos1);
         return repos2;
     }
-    
+
     render() {
         const { isLoaded, repos, commits, commitData } = this.state;
-      
+
         return (
-            <div className="App"> 
-            <div className="searchBar"> 
-            <Form getUser={this.getUser} />
-            </div> 
-            {!isLoaded ? <div> Enter Github user </div>: 
-            <div> 
-                <Graph data={commitData} />
-   
-                <br />
-            </div>
-            } 
-         
+            <div className="App">
+                <div className="searchBar">
+                    <Form getUser={this.getUser} />
+                </div>
+                {!isLoaded ? <div> Enter Github user </div> :
+                    <div>
+                        <Graph data={commitData} />
+
+                        <br />
+                    </div>
+                }
+
             </div>
 
         );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Form = props => (
   <form onSubmit={props.getUser}>
-    <input  type="text" name="userName" />
+    <input type="text" name="userName" placeholder="user name"/>
+    <input type="text" name="apiKey" placeholder="your api key"/>
     <button >Search</button>
   </form>
 );
